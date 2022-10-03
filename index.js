6c09542a09fdd8151a431a417761c1b79dfe74c8
alsumukhamedova <alsumukhamedova@yandex.ru>
2022-10-03 19:18:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('button_up').onclick = function () {
+    document.getElementById('first__picture_part').classList.toggle('first__picture_up');
+}
+document.getElementById('button_down').onclick = function () {
+    document.getElementById('first__picture_part').classList.toggle('first__picture_down');
+}
